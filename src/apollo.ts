4251c857e94da5743ced92b7fbe761a548e44fdb
nolec <ca10205@naver.com>
2020-05-08 01:59:10 +0900

    apollo

---
+++ src/apollo.ts
@@ -0,0 +1,19 @@
+import ApolloClient, { Operation } from "apollo-boost";
+
+const client = new ApolloClient({
+  clientState: {
+    defaults: {
+      auth: {
+        _typename: "Auth",
+      },
+    },
+  },
+  request: async (operation: Operation) => {
+    operation.setContext({
+      headers: { "X-JWT": localStorage.getItem("jwt") || "" },
+    });
+  },
+  uri: "http://localhost:5000/graphql",
+});
+
+export default client;
